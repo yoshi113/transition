59c1a1d5860e43763f1f94ff454e48faf114affe
bb1 <bb1@bb1.local>
2021-01-23 15:26:45 -0600

    add list

--- src/pages/TransitionGroup/index.js
+++ src/pages/TransitionGroup/index.js
@@ -1,5 +1,30 @@
+import React from "react";
+import { Button, Item } from "./styles";
+
+let id = 1;
+
 const Page = () => {
-  return <div></div>;
+  const [items, setItems] = React.useState([]);
+
+  const handleAdd = () => {
+    setItems([id++, ...items]);
+  };
+
+  const handleRemove = (id) => {
+    setItems(items.filter((item) => item !== id));
+  };
+
+  return (
+    <>
+      <Button onClick={handleAdd}>Add</Button>
+      {items.map((item) => (
+        <Item key={item}>
+          Item {item}
+          <span onClick={() => handleRemove(item)}>x</span>
+        </Item>
+      ))}
+    </>
+  );
 };
 
 export default Page;
--- src/pages/TransitionGroup/styles.js
+++ src/pages/TransitionGroup/styles.js
@@ -1,1 +1,27 @@
 import styled from "styled-components";
+
+export const Button = styled.div`
+  width: 150px;
+  margin-bottom: 50px;
+  padding: 10px 30px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 190px;
+  height: 40px;
+  padding-left: 20px;
+  border: 1px solid #ccc;
+  margin-bottom: -1px;
+
+  span {
+    padding: 5px 10px;
+    line-height: 1;
+    opacity: 0.5;
+    cursor: pointer;
+  }
+`;
